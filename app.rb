a233b6434c99cfaa495b8c2620496eaae8b102a0
Marco A. Peraza <marcoaperaza@gmail.com>
2016-10-25 01:50:24 -0400

    Add scopes for bot

--- app.rb
+++ app.rb
@@ -42,7 +42,7 @@ get '/oauth' do
 end
 
 get '/authorize' do
-  redirect "https://slack.com/oauth/authorize?client_id=#{ENV['SLACK_CLIENT_ID']}&scope=channels:history channels:write channels:read files:read files:write:user groups:history groups:read groups:write"
+  redirect "https://slack.com/oauth/authorize?client_id=#{ENV['SLACK_CLIENT_ID']}&scope=channels:history channels:write channels:read files:read files:write:user groups:history groups:read groups:write users:read bot"
 end
 
 post '/event' do
