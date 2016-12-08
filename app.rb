e1a08f72e4d9a37f11e071d1834de6975c8baf2e
Marco A. Peraza <marcoaperaza@gmail.com>
2016-12-08 14:41:14 -0500

    Move to modular sinatra application

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'sinatra/activerecord'
 require './environments'
 require 'json'
@@ -9,140 +9,145 @@ require './insults'
 
 def random_pic_path
   File.join('pics', Dir.entries('public/pics').select { |f| f =~ /.*\.jpg/ }.sample)
-
 end
 
-get '/oauth' do
-  result = HTTParty.post('https://slack.com/api/oauth.access',
-                         body: {
-                            client_id: ENV['SLACK_CLIENT_ID'],
-                            client_secret: ENV['SLACK_CLIENT_SECRET'],
-                            code: params['code']
-                         })
+class TrumpEndPoints < Sinatra::Application
 
-  user = User.find_or_create_by(user_id: result['user_id'],
+  register Sinatra::ActiveRecordExtension
+
+  get '/oauth' do
+    result = HTTParty.post('https://slack.com/api/oauth.access',
+                          body: {
+                              client_id: ENV['SLACK_CLIENT_ID'],
+                              client_secret: ENV['SLACK_CLIENT_SECRET'],
+                              code: params['code']
+                          })
+
+    user = User.find_or_create_by(user_id: result['user_id'],
+                                  team_id: result['team_id'])
+    user.access_token = result['access_token']
+    user.scope = result['scope']
+    user.save
+
+    bot = Bot.find_or_create_by(user_id: result['bot']['bot_user_id'],
                                 team_id: result['team_id'])
-  user.access_token = result['access_token']
-  user.scope = result['scope']
-  user.save
-
-  bot = Bot.find_or_create_by(user_id: result['bot']['bot_user_id'],
-                              team_id: result['team_id'])
-  bot.access_token = result['bot']['bot_access_token']
-  bot.scope = result['scope']
-  bot.save
-end
-
-get '/authorize' do
-  redirect "https://slack.com/oauth/authorize?client_id=#{ENV['SLACK_CLIENT_ID']}&scope=channels:history channels:write channels:read files:read files:write:user groups:history groups:read groups:write users:read bot commands"
-end
-
-post '/event' do
-  request.body.rewind
-  raw_body = request.body.read
-  data = JSON.parse(raw_body)
-
-  if data['token'] != ENV['SLACK_VERIFY_TOKEN']
-    halt 403, 'Incorrect slack token'
+    bot.access_token = result['bot']['bot_access_token']
+    bot.scope = result['scope']
+    bot.save
   end
 
-  case data['type']
-  when 'url_verification'
-    content_type :json
-    return {challenge: data['challenge']}.to_json
+  get '/authorize' do
+    redirect "https://slack.com/oauth/authorize?client_id=#{ENV['SLACK_CLIENT_ID']}&scope=channels:history channels:write channels:read files:read files:write:user groups:history groups:read groups:write users:read bot commands"
   end
 
-  return 200
-end
+  post '/event' do
+    request.body.rewind
+    raw_body = request.body.read
+    data = JSON.parse(raw_body)
 
-post '/interact' do
-  payload = JSON.parse(params[:payload])
-  puts payload
+    if data['token'] != ENV['SLACK_VERIFY_TOKEN']
+      halt 403, 'Incorrect slack token'
+    end
 
-  action_name, action_value = payload['actions'][0]['name'], payload['actions'][0]['value'] if payload['actions'] && payload['actions'][0]
+    case data['type']
+    when 'url_verification'
+      content_type :json
+      return {challenge: data['challenge']}.to_json
+    end
 
-  case payload['callback_id']
-  when 'insult_callback'
+    return 200
+  end
 
-    channel = if (payload['channel']['name'] != 'privategroup')
-                "\##{payload['channel']['name']}"
-              else
-                "that #{payload['team']['domain']} channel"
-              end
+  post '/interact' do
+    payload = JSON.parse(params[:payload])
+    puts payload
 
-    insult = InsultTemplates[action_name].sample % { target: action_value,
-                                                     channel: channel,
-                                                     caller: payload['user']['name'] }
+    action_name, action_value = payload['actions'][0]['name'], payload['actions'][0]['value'] if payload['actions'] && payload['actions'][0]
 
-    HTTParty.post(payload['response_url'],
+    case payload['callback_id']
+    when 'insult_callback'
+
+      channel = if (payload['channel']['name'] != 'privategroup')
+                  "\##{payload['channel']['name']}"
+                else
+                  "that #{payload['team']['domain']} channel"
+                end
+
+      insult = InsultTemplates[action_name].sample % { target: action_value,
+                                                      channel: channel,
+                                                      caller: payload['user']['name'] }
+
+      HTTParty.post(payload['response_url'],
+                    body: {
+                      delete_original: true,
+                      response_type: "in_channel",
+                      username: 'Donald J. Trump',
+                      link_names: '1',
+                      attachments: [
+                        {
+                          fallback: insult,
+                          text: insult,
+                          image_url: url(random_pic_path),
+                          color: '#d83924'
+                        }
+                      ]
+                    }.to_json,
+                    headers: { 'Content-Type' => 'application/json' })
+    end
+
+    status 200
+  end
+
+  post '/insult' do
+    if params[:ssl_check] == '1'
+      halt 200
+    end
+
+    if params[:token] != ENV["SLACK_VERIFY_TOKEN"]
+      halt 403, "Incorrect slack token"
+    end
+
+    target = params[:text]
+
+
+    HTTParty.post(params[:response_url],
                   body: {
-                    delete_original: true,
-                    response_type: "in_channel",
-                    username: 'Donald J. Trump',
-                    link_names: '1',
-                    attachments: [
-                      {
-                        fallback: insult,
-                        text: insult,
-                        image_url: url(random_pic_path),
-                        color: '#d83924'
-                      }
-                    ]
-                  }.to_json,
+                          attachments: [
+                            {
+                              fallback: 'Error: Your Slack client does not support the necessary features',
+                              text: 'Choose a kind of insult',
+                              callback_id: 'insult_callback',
+                              actions: [
+                                {
+                                  name: 'low_energy',
+                                  text: 'Low Energy',
+                                  type: 'button',
+                                  value: target
+                                },
+                                {
+                                  name: 'loser',
+                                  text: 'Loser',
+                                  type: 'button',
+                                  value: target
+                                },
+                                {
+                                  name: 'dishonest',
+                                  text: 'Dishonest',
+                                  type: 'button',
+                                  value: target
+                                },
+                                {
+                                  name: 'dumb',
+                                  text: 'Very Dumb',
+                                  type: 'button',
+                                  value: target
+                                }
+                              ]
+                            }
+                          ]
+                        }.to_json,
                   headers: { 'Content-Type' => 'application/json' })
+    status 200
   end
 
-  status 200
-end
-
-post '/insult' do
-  if params[:ssl_check] == '1'
-    halt 200
-  end
-
-  if params[:token] != ENV["SLACK_VERIFY_TOKEN"]
-    halt 403, "Incorrect slack token"
-  end
-
-  target = params[:text]
-
-
-  HTTParty.post(params[:response_url],
-                body: {
-                        attachments: [
-                          {
-                            fallback: 'Error: Your Slack client does not support the necessary features',
-                            text: 'Choose a kind of insult',
-                            callback_id: 'insult_callback',
-                            actions: [
-                              {
-                                name: 'low_energy',
-                                text: 'Low Energy',
-                                type: 'button',
-                                value: target
-                              },
-                              {
-                                name: 'loser',
-                                text: 'Loser',
-                                type: 'button',
-                                value: target
-                              },
-                              {
-                                name: 'dishonest',
-                                text: 'Dishonest',
-                                type: 'button',
-                                value: target
-                              },
-                              {
-                                name: 'dumb',
-                                text: 'Very Dumb',
-                                type: 'button',
-                                value: target
-                              }
-                            ]
-                          }
-                        ]
-                      }.to_json,
-                headers: { 'Content-Type' => 'application/json' })
-  status 200
 end
